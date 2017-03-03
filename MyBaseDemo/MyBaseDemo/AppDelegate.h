f6437245107e2561c2ab70d989673d7137465bc1
madliufeng <1318122887@qq.com>
2017-03-03 20:04:13 +0800

    初始化项目

---
+++ MyBaseDemo/MyBaseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBaseDemo
+//
+//  Created by Pro on 17/3/3.
+//  Copyright © 2017年 Pro_liufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
